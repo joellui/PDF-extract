019986c4eb0bf048e37ae50fa88d7a874fb5a1ef
Joel Louis <joellouis2000@gmail.com>
2022-12-23 19:21:23 +0530

    Major Update - Created page for upload - created list view for all files - add data to postgres (save in text pending)

--- pdfExtract/extractApp/models.py
+++ pdfExtract/extractApp/models.py
@@ -3,3 +3,11 @@ from django.db import models
 # Create your models here.
 class extractApp(models.Model):
     content = models.TextField()
+
+class pdf(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    pdf = models.FileField(upload_to='pdfs/')
+
+    def __str__(self):
+        return self.title
--- pdfExtract/extractApp/views.py
+++ pdfExtract/extractApp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+
+from .models import pdf
+
+def home(request):
+    pdfs = pdf.objects.all()
+    return render(request, 'home.html', {
+        'pdfs': pdfs
+    })
 
 # Create your views here.
+def upload(request):
+    context = {}
+    if request.method == 'POST':
+        upload_file = request.FILES['document']
+        fs =  FileSystemStorage()
+        name = fs.save(upload_file.name, upload_file)
+        context['url'] = fs.url(name)
+        pdf.objects.create(title=name, content='', pdf=context['url'])
+    return render(request, 'upload.html', context)
+
